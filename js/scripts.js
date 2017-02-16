d9a9e520ee1e535d7a90c7aeabfdc53c225bfdcb
Tom Yoder and Zach Beecher <tom@>
2017-02-16 10:11:55 -0800

    add id to div that shows none, when user types animal

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function() {
+  var animal = prompt("Type your favorite animal. Turtles, Ferrets, or Squirrels?");
+
+  if (animal === "Turtles") {
+    $('#turtle').show();
+  }
+    else if
+    (animal ==="Ferrets"){
+      $("#ferret").show();
+
+
+  }
+    else if
+    (animal ==="Squirrels"){
+      ("#squirrel").show();
+
+  }
+    else {
+    prompt("Type your favorite animal. Turtles, Ferrets, or Squirrels?");
+  }
+
+});
